0530a52a6d72e9c819755de032a764f06780b9fc
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2022-05-23 19:24:36 -0300

    🔥 remove unused code generated by CRA

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import ContactForm from './components/Contact/ContactForm';
+import { ContactForm } from './components/Contact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
       <ContactForm />
     </div>
   );
