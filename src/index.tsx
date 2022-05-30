bb126a932718743ef627b2dcda1f5d5a4299aa7c
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2022-05-30 02:24:18 -0300

    🔥 remove App component

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,15 @@ import './index.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Router from './routing/Router';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router />
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
